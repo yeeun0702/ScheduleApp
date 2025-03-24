0f77241b9fb10ed9a846ee809622810832d6bfe6
yeeun0702 <kimyeeun0702@naver.com>
2025-03-24 19:22:46 +0900

    ✨ [feat] #1 Lv 5. 공통 응답 클래스 및 응답 코드 Enum 추가

---
+++ src/main/java/com/example/scheduleapp/common/response/ApiResponseDto.java
@@ -0,0 +1,47 @@
+package com.example.scheduleapp.common.response;
+
+import com.example.scheduleapp.common.response.enums.ErrorCode;
+import com.example.scheduleapp.common.response.enums.SuccessCode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.annotation.Nullable;
+import lombok.Builder;
+import lombok.NonNull;
+import org.springframework.http.HttpStatus;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Builder
+public record ApiResponseDto<T>(
+        @JsonIgnore HttpStatus httpStatus,
+        int code,
+        @NonNull String message,
+        @JsonInclude(value = NON_NULL) T data
+) {
+    public static <T> ApiResponseDto<T> success(final SuccessCode successCode, @Nullable final T data) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(successCode.getHttpStatus())
+                .code(successCode.getCode())
+                .message(successCode.getMessage())
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponseDto<T> success(final SuccessCode successCode) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(successCode.getHttpStatus())
+                .code(successCode.getCode())
+                .message(successCode.getMessage())
+                .data(null)
+                .build();
+    }
+
+    public static <T> ApiResponseDto<T> fail(final ErrorCode errorCode) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(errorCode.getHttpStatus())
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .data(null)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/scheduleapp/common/response/enums/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.example.scheduleapp.common.response.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    //400 Bad Request
+    BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "유효한 요청이 아닙니다."),
+    MISSING_REQUIRED_HEADER(400, HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
+    MISSING_REQUIRED_PARAMETER(400, HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
+    //404 Not Found
+    NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
+    SCHEDULE_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 일정입니다."),
+    USER_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 User Id 입니다."),
+    //405 Method Not Allowed
+    METHOD_NOT_ALLOWED(405, HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다."),
+    //500 Internal Server Error
+    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+
+    private final int code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
