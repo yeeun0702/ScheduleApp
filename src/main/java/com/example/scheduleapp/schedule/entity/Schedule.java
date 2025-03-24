8ebada2a0db5bd563150cccb1f78a774982f892d
yeeun0702 <kimyeeun0702@naver.com>
2025-03-24 19:17:52 +0900

    ✨ [feat] #1 Lv 1. Schedule 엔티티 생성

---
+++ src/main/java/com/example/scheduleapp/schedule/entity/Schedule.java
@@ -0,0 +1,30 @@
+package com.example.scheduleapp.schedule.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+    private Long id;
+    private Long userId;
+    private String userName;
+    private String todo;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // 생성자
+    public Schedule(String userName, String todo, String password) {
+        this.userName = userName;
+        this.todo = todo;
+        this.password = password;
+    }
+
+}
