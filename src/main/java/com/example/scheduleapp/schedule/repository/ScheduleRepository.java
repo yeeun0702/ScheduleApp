2dc0ce50d32ff4391ec9ebeb5ccf351ea7640823
yeeun0702 <kimyeeun0702@naver.com>
2025-03-24 22:12:53 +0900

    ✨ [feat] #3 일정 전체 조회 DB 저장 로직 구현

--- src/main/java/com/example/scheduleapp/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduleapp/schedule/repository/ScheduleRepository.java
@@ -1,9 +1,15 @@
 package com.example.scheduleapp.schedule.repository;
 
 import com.example.scheduleapp.schedule.dto.response.ScheduleDto;
+import com.example.scheduleapp.schedule.dto.response.ScheduleListDto;
 import com.example.scheduleapp.schedule.entity.Schedule;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScheduleRepository {
 
     ScheduleDto createSchedule(Schedule schedule);
+
+    List<ScheduleListDto> getAllSchedules(String userName, LocalDateTime updatedAt);
 }
