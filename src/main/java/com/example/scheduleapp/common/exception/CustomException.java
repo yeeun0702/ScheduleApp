bdf4da95ac019a6a4a3289eeb566378a8faea4b3
yeeun0702 <kimyeeun0702@naver.com>
2025-03-24 19:28:57 +0900

    ✨ [feat] #1 Lv 5. 전역 예외 처리 및 커스텀 예외 클래스 추가

---
+++ src/main/java/com/example/scheduleapp/common/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.example.scheduleapp.common.exception;
+
+import com.example.scheduleapp.common.response.enums.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return errorCode.getHttpStatus();
+    }
+}
