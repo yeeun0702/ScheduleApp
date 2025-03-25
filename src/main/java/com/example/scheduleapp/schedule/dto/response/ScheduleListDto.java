a3593396c9350d580407c759ea945df5d41d58bb
yeeun0702 <kimyeeun0702@naver.com>
2025-03-26 01:59:36 +0900

    ♻️ [refactor] #13 조회 응답에 작성자 이메일 정보 추가

--- src/main/java/com/example/scheduleapp/schedule/dto/response/ScheduleListDto.java
+++ src/main/java/com/example/scheduleapp/schedule/dto/response/ScheduleListDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record ScheduleListDto(
         long id,
         String userName,
+        String email,
         String todo,
         LocalDateTime updatedAt
 ) {
