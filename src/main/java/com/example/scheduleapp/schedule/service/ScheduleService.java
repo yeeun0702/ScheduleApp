11986fa156da5d2a17cfb9cf9c59ec79c055ff46
yeeun0702 <kimyeeun0702@naver.com>
2025-03-26 13:34:58 +0900

    ♻️ [refactor] #15 서비스 계층에 페이지네이션 로직 적용

--- src/main/java/com/example/scheduleapp/schedule/service/ScheduleService.java
+++ src/main/java/com/example/scheduleapp/schedule/service/ScheduleService.java
@@ -1,7 +1,9 @@
 package com.example.scheduleapp.schedule.service;
 
+import com.example.scheduleapp.schedule.dto.request.PageRequestDto;
 import com.example.scheduleapp.schedule.dto.request.ScheduleCreateDto;
 import com.example.scheduleapp.schedule.dto.request.ScheduleUpdateDto;
+import com.example.scheduleapp.schedule.dto.response.PageResponseDto;
 import com.example.scheduleapp.schedule.dto.response.ScheduleDetailDto;
 import com.example.scheduleapp.schedule.dto.response.ScheduleDto;
 import com.example.scheduleapp.schedule.dto.response.ScheduleListDto;
@@ -14,7 +16,8 @@ public interface ScheduleService {
 
     ScheduleDto createSchedule(ScheduleCreateDto scheduleCreateDto); // 메모 생성
 
-    List<ScheduleListDto> getAllSchedules(Long userId, LocalDateTime updatedAt); // 메모 전체 조회 (userId로 조회)
+    // 메모 전체 조회 (userId로 조회) + 페이지네이션 적용
+    PageResponseDto<ScheduleListDto> getAllSchedules(Long userId, LocalDateTime updatedAt, PageRequestDto pageRequestDto);
 
     ScheduleDetailDto getDetailSchedule(long scheduleId); // 메모 상세 조회
 
