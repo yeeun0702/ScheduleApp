a222ec5cad62141dfcdaf68d48cfcda4afbbc95d
yeeun0702 <kimyeeun0702@naver.com>
2025-03-27 20:11:27 +0900

    🔥 [chore] #17 실수로 올린 클래스들 삭제

--- src/main/java/com/example/scheduleapp/domain/users/dto/request/ScheduleDeleteDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.scheduleapp.domain.users.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ScheduleDeleteDto(
-        @NotBlank String password
-) {
-}
