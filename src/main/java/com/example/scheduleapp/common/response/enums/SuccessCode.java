f6f6885465cd4988b0a2bd19be0d6ce3a78fec18
yeeun0702 <kimyeeun0702@naver.com>
2025-03-27 20:15:41 +0900

    ♻️ [refactor] #17 HTTP 상태와 구분되는 도메인 기반 에러 코드로 변경

--- src/main/java/com/example/scheduleapp/common/response/enums/SuccessCode.java
+++ src/main/java/com/example/scheduleapp/common/response/enums/SuccessCode.java
@@ -8,15 +8,17 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    // 201 created
-    SCHEDULE_POST_SUCCESS(201, HttpStatus.CREATED, "일정이 성공적으로 생성되었습니다."),
+    // 201 Created
+    SCHEDULE_CREATE_SUCCESS("SS2010", HttpStatus.CREATED, "일정이 성공적으로 생성되었습니다."),
 
     // 200 OK
-    SCHEDULE_GET_SUCCESS(200, HttpStatus.OK, "일정을 성공적으로 찾았습니다."),
-    SCHEDULE_LIST_FOUND(200, HttpStatus.OK, "일정 목록을 성공적으로 찾았습니다"),
-    SCHEDULE_PUT_SUCCESS(200, HttpStatus.OK, "일정을 성공적으로 수정했습니다.");
+    SCHEDULE_READ_SUCCESS("SS2000", HttpStatus.OK, "일정을 성공적으로 조회했습니다."),
+    SCHEDULE_LIST_SUCCESS("SS2001", HttpStatus.OK, "일정 목록을 성공적으로 조회했습니다."),
+    SCHEDULE_UPDATE_SUCCESS("SS2002", HttpStatus.OK, "일정을 성공적으로 수정했습니다."),
+    SCHEDULE_DELETE_SUCCESS("SS2003", HttpStatus.OK, "일정을 성공적으로 삭제했습니다.");
 
-    private final int code;
+    private final String code;
     private final HttpStatus httpStatus;
     private final String message;
 }
+
